3ce577b7dc844612a979f2778be17c02832d5c0e
Rioh <90512887+Rioh1118@users.noreply.github.com>
2024-10-24 14:33:55 +0900

    sample1にjfifの変換を追加

--- sample1.py
+++ sample1.py
@@ -6,6 +6,7 @@ from road_sign import DatasetHandler
 
 data_handler = DatasetHandler(data_dir="./data/project18", train_dir="./data/train", test_dir="./data/test")
 
+data_handler.convert_jfif_to_jpg_or_png()
 data_handler.load_data()
 data_handler.split_data()
 data_handler.save_data()
